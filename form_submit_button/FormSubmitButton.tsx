539258240f659994c338290c2757ddde7538a15a
kadirtikil <kadirtikil@gmail.com>
2024-12-06 19:00:27 +0100

    initial push

---
+++ form_submit_button/FormSubmitButton.tsx
@@ -0,0 +1,25 @@
+import './FormSubmitButton.css'
+
+type FormSubmitButtonProps = {
+    button_height: number,
+    button_width: number,
+    button_color: string,
+    placeholder: string,
+}
+
+
+export default function FormSubmitButton({button_height, button_width, button_color, placeholder}: FormSubmitButtonProps) {
+
+    return(
+        <div className="text">
+            <button 
+                style={{height: button_height, width: button_width, color: button_color, fontSize: button_height / 2}}
+                className='button'
+                id='text'
+            >
+                {placeholder}                
+            </button>
+        </div>
+    )
+
+}
